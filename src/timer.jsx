7e87b8500ecc3415159a5266359ee4950994fee3
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-10 11:51:53 -0400

    final commit

--- src/timer.jsx
+++ src/timer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import App from "./App.css";
 const Timer = ({ title }) => {
   const [elapsedTime, setElapsedTime] = useState(0);
   useEffect(() => {
